daf6c962430dc05c2b19ea4ca1aa2a3a4625749b
nimurr <nimurnerob404@gmail.com>
2025-11-13 19:33:42 +0600

    `Added enum Role and User type with properties`

--- module6.ts
+++ module6.ts
@@ -74,3 +74,32 @@
 
 // console.log(add({ name: "John"})) //? get error because age is missing
 // console.log(add({ name: "John", age: 30 }))
+
+//! video 8
+
+
+//? use enum for role 
+
+// enum Role {
+//     USER = "user",
+//     ADMIN = "admin"
+// }
+
+// type User = {
+//     name: string,
+//     age: number,
+//     email: string,
+//     password: string,
+//     role: Role
+// }
+
+// const user: User = {
+//     name: "John",
+//     age: 30,
+//     email: "0d4lK@example.com",
+//     password: "lkdsfhasldhfsdf45",
+//     role: Role.USER
+// }
+
+
+// console.log(user)
